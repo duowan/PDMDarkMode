133fbdc6b1601f267197351016134cc38c0f6509
PonyCui <cuiminghui@yy.com>
2015-03-12 10:05:24 +0800

    add progressView style

---
+++ PDMDarkMode/Classes/Application Logic/Render/UIProgressView+PDMRender.h
@@ -0,0 +1,14 @@
+//
+//  UIProgressView+PDMRender.h
+//  PDMDarkMode
+//
+//  Created by 崔 明辉 on 15/3/12.
+//  Copyright (c) 2015年 多玩事业部 iOS开发组 YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIView+PDMRender.h"
+
+@interface UIProgressView (PDMRender)
+
+@end
---
+++ PDMDarkMode/Classes/Application Logic/Render/UIProgressView+PDMRender.m
@@ -0,0 +1,48 @@
+//
+//  UIProgressView+PDMRender.m
+//  PDMDarkMode
+//
+//  Created by 崔 明辉 on 15/3/12.
+//  Copyright (c) 2015年 多玩事业部 iOS开发组 YY Inc. All rights reserved.
+//
+
+#import "UIProgressView+PDMRender.h"
+
+@implementation UIProgressView (PDMRender)
+
+- (BOOL)pdm_rendWithSkinItem:(PDMSkinItem *)skinItem {
+    if (![super pdm_rendWithSkinItem:skinItem]) {
+        return NO;
+    }
+    {
+        UIColor *progressTintColor = self.progressTintColor;
+        [skinItem colorItemWithOriginColor:progressTintColor withCompletionBlock:^(PDMColorItem *foundItem) {
+            if (foundItem != nil) {
+                self.progressTintColor = foundItem.replacingColor;
+                [self setObject:progressTintColor forRestoreKey:@"progressTintColor"];
+            }
+        }];
+    }
+    {
+        UIColor *trackTintColor = self.trackTintColor;
+        [skinItem colorItemWithOriginColor:trackTintColor withCompletionBlock:^(PDMColorItem *foundItem) {
+            if (foundItem != nil) {
+                self.trackTintColor = foundItem.replacingColor;
+                [self setObject:trackTintColor forRestoreKey:@"trackTintColor"];
+            }
+        }];
+    }
+    return YES;
+}
+
+- (void)pdm_restore {
+    [super pdm_restore];
+    if (self.pdm_restoreData[@"progressTintColor"]) {
+        self.trackTintColor = self.pdm_restoreData[@"progressTintColor"];
+    }
+    if (self.pdm_restoreData[@"trackTintColor"]) {
+        self.trackTintColor = self.pdm_restoreData[@"trackTintColor"];
+    }
+}
+
+@end
